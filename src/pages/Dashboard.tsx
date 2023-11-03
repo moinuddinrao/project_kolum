94023181e9a2887e74e02811c54cc88f4fd578f4
Moin ud din <moinrajput594@gmail.com>
2023-11-03 18:30:32 +0500

    feat: complete Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 
-import { EditOutlined, CloseOutlined } from "@ant-design/icons";
+import { QuestionCircleOutlined, DownOutlined } from "@ant-design/icons";
 
 import { PrimaryButton } from "@/components/Button/PrimaryButton";
 import styles from "@/assets/Styles";
+import CustomForm from "@/components/Form/CustomForm";
 
 interface Company {
   name: string;
   eori: string;
-  portalId: string | null;
+  portalId: number | null;
   streetName: string | null;
   streetNumber: string | null;
   addressAddition: string | null;
@@ -18,18 +19,12 @@ interface Company {
   poBox: string | null;
 }
 
-const Dashboard = () => {
-  const [updataLegalInfo, setUpdateLegalInfo] = useState(false);
-  const [updateAddress, setUpdateAddress] = useState(false);
-
-  const saveData = () => {
-    console.log("Data Saved");
-  };
-
-  const companyData: Company = {
+const Dashboard: React.FC = () => {
+  // State to track form values
+  const [initialData, setInitialData] = useState<Company>({
     name: "kolum.earth",
     eori: "DE123456789",
-    portalId: "123456789",
+    portalId: 123456789,
     streetName: "Kolum Street",
     streetNumber: "1",
     addressAddition: "1st Floor",
@@ -37,137 +32,56 @@ const Dashboard = () => {
     zip: 12345,
     country: "Germany",
     poBox: null,
+  });
+
+  const handleLegalInfoSubmit = (values: Partial<Company>) => {
+    // Handle legal information form submission here
+    setInitialData({ ...initialData, ...values });
+    console.log("Legal Information Updated:", values);
+  };
+
+  const handleAddressSubmit = (values: Partial<Company>) => {
+    // Handle address form submission here
+    setInitialData({ ...initialData, ...values });
+    console.log("Address Updated:", values);
   };
 
   return (
-    // Company Data Section
     <section id="company-data" className={`${styles.section}`}>
       <h1 className={`${styles.heading1}`}>Company Data</h1>
 
-      {/* Box Start */}
       <div className={`${styles.box}`}>
-        {/* Company Name */}
         <h2 className={`${styles.heading2}`}>kolum.earth</h2>
 
         {/* Legal Information */}
-        <div className={`flex flex-col gap-5`}>
-          <div className="flex items-center justify-between">
-            <h3 className={`${styles.heading3}`}>Legal Information</h3>
-            {companyData && (
-              <PrimaryButton
-                onClick={() => {
-                  setUpdateLegalInfo(!updataLegalInfo);
-                }}
-                className="w-10 h-10 flex justify-center items-center text-xl"
-              >
-                {updataLegalInfo ? <CloseOutlined /> : <EditOutlined />}
-              </PrimaryButton>
-            )}
-          </div>
+        <CustomForm<Company>
+          formValues={initialData}
+          onSubmit={handleLegalInfoSubmit}
+          title="Legal Information"
+          fields={[
+            { label: "Legal Name", name: "name", required: true },
+            { label: "EORI Number", name: "eori", required: true },
+            { label: "Trader Portal ID Number", name: "portalId" },
+          ]}
+        />
 
-          {/* View Legal Information */}
-          <div className={`${updataLegalInfo ? "hidden" : "block"} flex`}>
-            {/* Legal Name */}
-            <div className="w-1/3 flex flex-col items-left gap-3">
-              <p className={`${styles.label}`}>Legal Name*</p>
-              <p className={`${styles.text}`}>{companyData.name}</p>
-            </div>
-
-            {/* EORI Number */}
-            <div className="w-1/3 flex flex-col items-left gap-3">
-              <p className={`${styles.label}`}>EORI Number*</p>
-              <p className={`${styles.text}`}>{companyData.eori}</p>
-            </div>
-
-            {/* Trader Portal ID Number*/}
-            <div className="w-1/3 flex flex-col items-left gap-3">
-              <p className={`${styles.label}`}>Trader Portal ID Number</p>
-              <p className={`${styles.text}`}>{companyData.portalId}</p>
-            </div>
-          </div>
-
-          {/* Update Legal Information */}
-          <div className={`${updataLegalInfo ? "block" : "hidden"}`}>
-            {/* <UpdateLegalInformation /> */}
-          </div>
-        </div>
-
-        {/* Address */}
-        <div className={`flex flex-col gap-5`}>
-          <div className="flex items-center justify-between">
-            <h3 className={`${styles.heading3}`}>Address</h3>
-            {companyData && (
-              <PrimaryButton
-                onClick={() => {
-                  setUpdateAddress(!updateAddress);
-                }}
-                className="w-10 h-10 flex justify-center items-center text-xl"
-              >
-                {updateAddress ? <CloseOutlined /> : <EditOutlined />}
-              </PrimaryButton>
-            )}
-          </div>
-
-          {/* View Address */}
-          <div
-            className={`${
-              updateAddress ? "hidden" : "block"
-            } flex flex-col gap-5`}
-          >
-            {/* Address First Row */}
-            <div className="flex">
-              {/* Street Name */}
-              <div className="w-1/3 flex flex-col items-left gap-3">
-                <p className={`${styles.label}`}>Street Name*</p>
-                <p className={`${styles.text}`}>{companyData.streetName}</p>
-              </div>
-
-              {/* Street Number */}
-              <div className="w-1/3 flex flex-col items-left gap-3">
-                <p className={`${styles.label}`}>Street Number*</p>
-                <p className={`${styles.text}`}>{companyData.streetNumber}</p>
-              </div>
-            </div>
-
-            {/* Address Second Row */}
-            <div className="flex">
-              {/* City */}
-              <div className="w-1/3 flex flex-col items-left gap-3">
-                <p className={`${styles.label}`}>City</p>
-                <p className={`${styles.text}`}>{companyData.city}</p>
-              </div>
-
-              {/* Zip */}
-              <div className="w-1/3 flex flex-col items-left gap-3">
-                <p className={`${styles.label}`}>Zip Code</p>
-                <p className={`${styles.text}`}>{companyData.zip}</p>
-              </div>
-            </div>
-
-            {/* Address Third Row */}
-            <div className="flex">
-              {/* Country */}
-              <div className="w-1/3 flex flex-col items-left gap-3">
-                <p className={`${styles.label}`}>Country</p>
-                <p className={`${styles.text}`}>{companyData.country}</p>
-              </div>
-
-              {/* PO Box */}
-              <div className="w-1/3 flex flex-col items-left gap-3">
-                <p className={`${styles.label}`}>PO Box</p>
-                <p className={`${styles.text}`}>{companyData.poBox}</p>
-              </div>
-            </div>
-          </div>
-
-          {/* Update Address */}
-          <div className={`${updateAddress ? "block" : "hidden"}`}>
-            {/* <UpdateAddress /> */}
-          </div>
-        </div>
+        {/* Address Data*/}
+        <CustomForm<Company>
+          formValues={initialData}
+          onSubmit={handleAddressSubmit}
+          title="Address Data"
+          fields={[
+            { label: "Street Name", name: "streetName", required: true },
+            { label: "Street Number", name: "streetNumber", required: true },
+            { label: "City", name: "city", required: true },
+            { label: "Zip Code", name: "zip", required: true },
+            { label: "Country", name: "country", required: true },
+            { label: "PO Box", name: "poBox" },
+          ]}
+        />
 
         {/* Line */}
-        <hr className="m-0 h-0.5 text-nao_blue" />
+        <hr className="m-0 border border-solid border-nao_light_gray" />
 
         {/* Team Members */}
         <div className={`flex flex-col gap-5`}>
@@ -183,32 +97,26 @@ const Dashboard = () => {
           </div>
 
           {/* Line */}
-          <hr className="m-0 h-0.5 text-nao_blue" />
+          <hr className="m-0 border border-solid border-nao_light_gray" />
 
           {/* Table Header */}
           <div className="flex">
             <p className={`${styles.label} w-1/3`}>User</p>
-            <p className={`${styles.label} w-1/3`}>Access Level ?</p>
+            <p className={`${styles.label} w-1/3`}>
+              Access Level <QuestionCircleOutlined />
+            </p>
           </div>
 
           {/* Line */}
-          <hr className="m-0 h-0.5 text-nao_blue" />
+          <hr className="m-0 border border-solid border-nao_light_gray" />
 
-          {/* Table Body */}
+          {/* Table Row */}
           <div className="flex">
             <p className={`${styles.text} w-1/3`}>John Doe</p>
-            <p className={`${styles.text} w-1/3`}>Admin</p>
+            <p className={`${styles.text} w-1/3`}>
+              Admin <DownOutlined className="text-sm" />
+            </p>
           </div>
-        </div>
-
-        {/* Save Button */}
-        <div className="flex justify-end">
-          <PrimaryButton
-            onClick={saveData}
-            className="w-fit h-fit px-10 text-lg"
-          >
-            Save
-          </PrimaryButton>
         </div>
       </div>
     </section>

4c8132aae600e6988dcffa111529b62f25120932
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2023-11-03 18:11:55 +0500

    fix: minimum height for box

--- src/assets/Styles/index.ts
+++ src/assets/Styles/index.ts
@@ -9,9 +9,9 @@ const styles = {
     "!m-0 !p-0 !font-lato !font-medium !text-[16px] !leading-[24px] !text-nao_middledark_gray",
   text: "!m-0 !p-0 !font-lato !font-medium !text-[16px] !leading-[24px] !text-nao_dark_gray",
 
-  section: `flex flex-col gap-10 m-10`,
+  section: `min-h-[90vh] flex flex-col gap-10 mx-10 my-[5vh]`,
 
-  box: "flex flex-col gap-10 p-10 bg-white rounded-lg",
+  box: "flex flex-1 flex-col gap-10 p-10 bg-white rounded-lg",
 };
 
 export default styles;

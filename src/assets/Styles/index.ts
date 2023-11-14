93d91db5f1e9c3582beb1f242372208bed0841aa
Moin ud din <moinrajput594@gmail.com>
2023-11-14 21:36:25 +0500

    refactor: update styles color

--- src/assets/Styles/index.ts
+++ src/assets/Styles/index.ts
@@ -1,13 +1,11 @@
 const styles = {
   heading1:
-    "m-0 p-0 font-ubuntu font-bold text-[48px] leading-[56px] text-nao_black",
-  heading2:
-    "m-0 p-0 font-ubuntu font-bold text-[32px] leading-[40px] text-nao_dark_gray",
-  heading3:
-    "m-0 p-0 font-ubuntu font-bold text-[24px] leading-[32px] text-nao_dark_gray",
+    "m-0 p-0 font-ubuntu font-bold text-[48px] leading-[56px] text-black",
+  heading2: "m-0 p-0 font-ubuntu font-bold text-[32px] leading-[40px]",
+  heading3: "m-0 p-0 font-ubuntu font-bold text-[24px] leading-[32px]",
   label:
-    "!m-0 !p-0 !font-lato !font-medium !text-[16px] !leading-[24px] !text-nao_middledark_gray",
-  text: "!m-0 !p-0 !font-lato !font-medium !text-[16px] !leading-[24px] !text-nao_dark_gray",
+    "!m-0 !p-0 !font-lato !font-semibold !text-[16px] !leading-[24px] !text-nao_black",
+  text: "!m-0 !p-0 !font-lato !font-medium !text-[16px] !leading-[24px]",
 
   section: `min-h-[90vh] flex flex-col gap-10 mx-10 my-[5vh]`,
 

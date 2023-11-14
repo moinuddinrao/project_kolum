e3260ea2e60d336180c800d842bb0cdfc200c05d
Moin ud din <moinrajput594@gmail.com>
2023-11-14 21:40:18 +0500

    feat: add theme for antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { ConfigProvider } from "antd";
 import { RouterProvider } from "react-router-dom";
 
 import "./App.css";
@@ -8,7 +9,23 @@ import Router from "./Router";
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={Router} />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#22577a",
+            colorPrimaryHover: "#38A3A5",
+            colorBorder: "#22577a",
+            colorPrimaryBg: "#E9ECEF",
+            colorPrimaryBgHover: "#E9ECEF",
+            borderRadius: 8,
+            colorText: "#495057",
+            colorTextSecondary: "#495057",
+            colorTextTertiary: "#6c757d",
+          },
+        }}
+      >
+        <RouterProvider router={Router} />
+      </ConfigProvider>
     </div>
   );
 }

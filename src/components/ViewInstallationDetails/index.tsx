a9d57ff114f5d89c729407ad3f0112c72216f853
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2023-11-16 21:16:29 +0500

    update: installation component

--- src/components/ViewInstallationDetails/index.tsx
+++ src/components/ViewInstallationDetails/index.tsx
@@ -1,89 +1,240 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { ArrowLeftOutlined, QuestionCircleOutlined } from "@ant-design/icons";
-import { Table } from "antd";
+import {
+  Button,
+  Drawer,
+  Dropdown,
+  Input,
+  Menu,
+  Popconfirm,
+  Space,
+  Table,
+} from "antd";
+import type { TableColumnsType } from "antd";
+import {
+  MoreOutlined,
+  EditTwoTone,
+  DeleteTwoTone,
+  PlusOutlined,
+  LinkOutlined,
+} from "@ant-design/icons";
 
-import { ProducedGoodsData, ProducedGoodsColumn } from "./InstallationData";
+import { InstallationData, InstallationColumn } from "./InstallationData";
 
+import { SecondaryButton } from "@/components/Button/SecondaryButton";
 import { PrimaryButton } from "@/components/Button/PrimaryButton";
 import styles from "@/assets/Styles";
-
-interface AddressField {
-  name: string;
-  label: string;
-  value: string | number;
-}
-
-interface InstallationInformations {
-  name: string;
-  city: string;
-  country: string;
-  installationID: string;
-  economicActivity: string;
-  "UN/LOCODE Number": string;
-  addressField: AddressField[];
-}
+import {
+  BasicInformation,
+  ContactData,
+} from "@/pages/AllOperators/AddNewOperator";
 
 interface InstallationInformationsProps {
-  selectedRow: Partial<InstallationInformations>;
-  setSelectedRow: any;
+  basicInformation: BasicInformation;
+  contactData: ContactData;
 }
 
 const ViewInstallationDetails: React.FC<InstallationInformationsProps> = ({
-  selectedRow,
-  setSelectedRow,
+  basicInformation,
+  contactData,
 }) => {
-  const handleBackClick = () => {
-    setSelectedRow(null);
-  };
+  const [nextStep, setNextStep] = useState(false);
+  const [data, setData] = useState(InstallationData);
+  const [open, setOpen] = useState(false);
 
   const infofields = [
-    { name: "legalName", label: "Legal Name*", value: selectedRow.name },
-    {
-      name: "Installation_ID",
-      label: (
-        <span>
-          Installation ID Number <QuestionCircleOutlined />
-        </span>
-      ),
-      value: selectedRow.installationID,
-    },
+    { name: "legalName", label: "Legal Name*", value: basicInformation.name },
     {
       name: "UN/LOCODE_Number",
       label: "UN/LOCODE Number",
-      value: selectedRow["UN/LOCODE Number"],
+      value: "DEBER",
     },
     {
       name: "economicActivity",
       label: "Economic Activity",
-      value: selectedRow.economicActivity,
+      value: "Economic",
     },
   ];
 
-  const addressFields = selectedRow.addressField
-    ? selectedRow.addressField.map((field) => ({
-        name: field.name,
-        label: field.label,
-        value: field.value,
-      }))
-    : [];
-
-  const geographicalFields = [
-    { name: "latitude", label: "Latitude", value: "41.40338" },
-    { name: "longitude", label: "Longitude", value: "2.17403" },
+  const contactFields = [
+    { name: "firstName", label: "First Name", value: contactData.firstName },
+    { name: "lastName", label: "Last Name", value: contactData.lastName },
+    { name: "email", label: "Email", value: contactData.email },
+    {
+      name: "phoneNumber",
+      label: "Phone Number",
+      value: contactData.phone_number,
+    },
   ];
+
+  const addressFields = [
+    { name: "street", label: "Street", value: basicInformation.streetName },
+    {
+      name: "streetNumber",
+      label: "Street Number",
+      value: basicInformation.streetNumber,
+    },
+    { name: "city", label: "City", value: basicInformation.city },
+    { name: "postcode", label: "Postcode", value: basicInformation.poBox },
+    {
+      name: "country",
+      label: "Country",
+      value: basicInformation.country,
+    },
+    { name: "poBox", label: "PO Box", value: basicInformation.poBox },
+  ];
+
+  const deleteRecord = (record: any) => {
+    const newData = data.filter((item) => item.key !== record.key);
+    setData(newData);
+    console.log("Delete clicked");
+  };
+
+  const showDrawer = () => {
+    setNextStep(false);
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const getMenu = (record: any) => {
+    return (
+      <Menu>
+        <Menu.Item
+          className={`${styles.text}`}
+          key="edit"
+          icon={<EditTwoTone />}
+          // onClick={() => showDrawer(record)}
+        >
+          Edit
+        </Menu.Item>
+        <Menu.Item
+          className={`${styles.text}`}
+          key="delete"
+          icon={<DeleteTwoTone />}
+        >
+          <Popconfirm
+            title="Sure to delete?"
+            onConfirm={() => deleteRecord(record)}
+          >
+            Delete
+          </Popconfirm>
+        </Menu.Item>
+      </Menu>
+    );
+  };
+
+  const expandedRowRender = (record: any) => {
+    const expandableColumns: TableColumnsType<any> = [
+      {
+        title: "CN Code",
+        dataIndex: "imported_good",
+        key: "imported_good",
+        render: (importedGood: string[]) => (
+          <div className="flex flex-col gap-5">
+            {importedGood.map((code, index) => (
+              <div key={index}>{code}</div>
+            ))}
+          </div>
+        ),
+      },
+      {
+        title: "Emission Data",
+        dataIndex: "emissionData",
+        key: "emissionData",
+        render: (emissionData: string[]) => (
+          <div className="flex flex-col gap-5">
+            {emissionData.map((data, index) =>
+              data === "Available" ? (
+                <PrimaryButton key={index}>{data}</PrimaryButton>
+              ) : (
+                <SecondaryButton key={index}>{data}</SecondaryButton>
+              ),
+            )}
+          </div>
+        ),
+      },
+      {
+        title: "Production Process",
+        dataIndex: "productionProcess",
+        key: "productionProcess",
+        render: (productionProcess: string[]) => (
+          <div className="flex flex-col gap-5">
+            {productionProcess.map((data, index) =>
+              data === "Available" ? (
+                <PrimaryButton key={index}>{data}</PrimaryButton>
+              ) : (
+                <SecondaryButton key={index}>{data}</SecondaryButton>
+              ),
+            )}
+          </div>
+        ),
+      },
+      {
+        title: "Carbon Price Paid Data",
+        dataIndex: "carbonPricePaidData",
+        key: "carbonPricePaidData",
+        render: (carbonPricePaidData: string[]) => (
+          <div className="flex flex-col gap-5">
+            {carbonPricePaidData.map((data, index) =>
+              data === "Available" ? (
+                <PrimaryButton key={index}>{data}</PrimaryButton>
+              ) : (
+                <SecondaryButton key={index}>{data}</SecondaryButton>
+              ),
+            )}
+          </div>
+        ),
+      },
+      {
+        dataIndex: "operation",
+        width: "5%",
+        render: (_: any, record: any) => {
+          return (
+            <Dropdown overlay={getMenu(record)} placement="bottomRight">
+              <Button
+                className="ant-dropdown-link"
+                onClick={(e) => e.preventDefault()}
+              >
+                <MoreOutlined />
+              </Button>
+            </Dropdown>
+          );
+        },
+      },
+    ];
+
+    const data = [record];
+
+    return (
+      <Table
+        bordered={false}
+        columns={expandableColumns}
+        dataSource={data}
+        pagination={false}
+        rowKey={(record) => record.toString()}
+      />
+    );
+  };
 
   return (
     <>
       <h1 className={`${styles.heading1}`}>Add New Operator</h1>
 
-      <div className={`${styles.box} gap-5`}>
+      <div className={`${styles.box} my-[5vh]`}>
         <div className="flex justify-between items-center">
-          <PrimaryButton onClick={handleBackClick}>
+          <PrimaryButton>
             <ArrowLeftOutlined />
           </PrimaryButton>
+          <PrimaryButton onClick={showDrawer}>
+            <PlusOutlined />
+            Request missing data from your Operator
+          </PrimaryButton>
         </div>
-        <h2 className={`${styles.heading2}`}>{selectedRow?.name}</h2>
+        <h2 className={`${styles.heading2}`}>{basicInformation?.name}</h2>
 
         {/* Basic Information */}
         <h3 className={`${styles.heading3}`}>Basic Information</h3>
@@ -99,9 +250,23 @@ const ViewInstallationDetails: React.FC<InstallationInformationsProps> = ({
           ))}
         </div>
 
+        {/* Contact Data */}
+        <h3 className={`${styles.heading3}`}>Contact Data</h3>
+        <div className="flex flex-wrap gap-y-10">
+          {contactFields.map((field) => (
+            <div
+              className="w-1/3 flex flex-col items-left gap-3"
+              key={String(field.name)}
+            >
+              <p className={`${styles.label}`}>{field.label}</p>
+              <p className={`${styles.text}`}>{field.value}</p>
+            </div>
+          ))}
+        </div>
+
         {/* Address Data */}
         <h3 className={`${styles.heading3}`}>Address Data</h3>
-        <div className="flex flex-col flex-wrap">
+        <div className="flex flex-col flex-wrap ">
           <div className="flex">
             {addressFields.slice(0, 3).map((field) => (
               <div
@@ -130,26 +295,12 @@ const ViewInstallationDetails: React.FC<InstallationInformationsProps> = ({
           </div>
         </div>
 
-        {/* Geographical Coordinates */}
-        <h3 className={`${styles.heading3}`}>Geographical Coordinates</h3>
-        <div className="flex flex-row flex-wrap gap-y-10">
-          {geographicalFields.map((field) => (
-            <div
-              className="w-1/3 flex flex-col items-left gap-3"
-              key={String(field.name)}
-            >
-              <p className={`${styles.label}`}>{field.label}</p>
-              <p className={`${styles.text}`}>{field.value}</p>
-            </div>
-          ))}
-        </div>
-
         {/* Line */}
         <hr className="m-0 border border-solid border-nao_light_gray" />
 
         {/* Produced Goods */}
         <h3 className={`${styles.heading3}`}>
-          Produced Goods <QuestionCircleOutlined className="text-sm" />
+          Installations <QuestionCircleOutlined className="text-sm" />
         </h3>
 
         {/* Line */}
@@ -157,11 +308,80 @@ const ViewInstallationDetails: React.FC<InstallationInformationsProps> = ({
 
         <Table
           className={`${styles.text}`}
-          dataSource={ProducedGoodsData}
-          columns={ProducedGoodsColumn}
+          dataSource={InstallationData}
+          columns={InstallationColumn}
+          expandable={{ expandedRowRender }}
           pagination={false}
         />
       </div>
+
+      <Drawer
+        className={`${styles.text}`}
+        title={
+          <h2 className={`${styles.heading2}`}>
+            Request missing Data from <br />
+            your Operator
+          </h2>
+        }
+        placement="right"
+        size="large"
+        open={open}
+        onClose={onClose}
+      >
+        {nextStep ? (
+          <div className={`${styles.box}`}>
+            <p className={`${styles.label}`}>Thank you, we are done here!</p>
+            <p className={`${styles.label}`}>
+              We will notify you once your operator has filled out the data.
+            </p>
+            <PrimaryButton className="w-fit h-fit !px-5" onClick={onClose}>
+              Okay, great
+            </PrimaryButton>
+          </div>
+        ) : (
+          <>
+            <p className={`${styles.label}`}>
+              You can use kolum to request data that is required for your CBAM
+              report but which you do not have access to directly from your
+              operator. <br /> <br />
+            </p>
+            <p className={`${styles.label}`}>
+              Please provide contact details for your operator below. Your
+              operator will receive a link to an online form where specific
+              information on the installations, emission and production data as
+              well carbon price paid can be added. All information is added to
+              our database and will be automatically attached to your operator
+              and considered in your generated report for each quarter. We will
+              let you know once the operator has filled in the data.
+            </p>
+
+            <div className="flex flex-col m-10 gap-5">
+              <Space.Compact>
+                <Input placeholder="Your contact person’s email" />
+                <Button type="primary"> Send Email</Button>
+              </Space.Compact>
+
+              <Space.Compact>
+                <Input
+                  placeholder="..or copy the invititation link "
+                  suffix={
+                    <Button type="primary" icon={<LinkOutlined />}>
+                      Copy Link
+                    </Button>
+                  }
+                />
+              </Space.Compact>
+            </div>
+
+            <PrimaryButton
+              className="w-fit h-fit !px-5"
+              onClick={() => setNextStep(true)}
+            >
+              Finish
+            </PrimaryButton>
+          </>
+        )}
+      </Drawer>
     </>
   );
 };

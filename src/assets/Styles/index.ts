67fda5f8f16a86ac58c5c69969879bd0a8eded49
Moin ud din <moinrajput594@gmail.com>
2023-11-16 22:07:28 +0500

    style: give color to headings and text

--- src/assets/Styles/index.ts
+++ src/assets/Styles/index.ts
@@ -1,11 +1,13 @@
 const styles = {
   heading1:
     "m-0 p-0 font-ubuntu font-bold text-[48px] leading-[56px] text-black",
-  heading2: "m-0 p-0 font-ubuntu font-bold text-[32px] leading-[40px]",
-  heading3: "m-0 p-0 font-ubuntu font-bold text-[24px] leading-[32px]",
+  heading2:
+    "m-0 p-0 font-ubuntu font-bold text-[32px] leading-[40px] text-black",
+  heading3:
+    "m-0 p-0 font-ubuntu font-bold text-[24px] leading-[32px] text-black",
   label:
-    "!m-0 !p-0 !font-lato !font-semibold !text-[16px] !leading-[24px] !text-nao_black",
-  text: "!m-0 !p-0 !font-lato !font-medium !text-[16px] !leading-[24px]",
+    "!m-0 !p-0 !font-lato !font-bold !text-[16px] !leading-[24px] !text-nao_black",
+  text: "!m-0 !p-0 !font-lato !font-medium !text-[16px] !leading-[24px] !text-nao_dark_gray",
 
   section: `min-h-[90vh] flex flex-col gap-10 mx-10 my-[5vh]`,
 

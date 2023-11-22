122164f7261abc40f8ea78e46637c6cc0cf6f3b2
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2023-11-22 20:58:07 +0500

    fix: input and select component

--- src/components/Collapse/InputCollapse.tsx
+++ src/components/Collapse/InputCollapse.tsx
@@ -1,53 +1,45 @@
 import React from "react";
 
-import { Collapse, Form, Input } from "antd";
+import { Form, Input } from "antd";
 
-const { Panel } = Collapse;
+const { TextArea } = Input;
 
 interface InputCollapseProps {
-  header: string;
-  label: string;
   fields: Array<{
     name: string;
     placeholder: string;
     required?: boolean;
     value: string | number | undefined;
+    inputType?: "input" | "textarea"; // Add this property to indicate input type
   }>;
-  onChange: (key: string | string[]) => void;
-  defaultActiveKey: string[];
 }
 
-const InputCollapse: React.FC<InputCollapseProps> = ({
-  header,
-  fields,
-  onChange,
-  defaultActiveKey,
-}) => {
+const InputCollapse: React.FC<InputCollapseProps> = ({ fields }) => {
   return (
-    <Collapse defaultActiveKey={defaultActiveKey} onChange={onChange}>
-      <Panel header={header} key="1">
-        <div className="w-full flex flex-col">
-          {fields.map((field) => (
-            <Form.Item
-              key={String(field.name)}
-              name={String(field.name)}
-              rules={
-                field.required
-                  ? [
-                      {
-                        required: true,
-                        message: `Please input your ${field.placeholder}!`,
-                      },
-                    ]
-                  : []
-              }
-            >
-              <Input placeholder={field.placeholder} />
-            </Form.Item>
-          ))}
-        </div>
-      </Panel>
-    </Collapse>
+    <div className="w-full flex flex-col">
+      {fields.map((field) => (
+        <Form.Item
+          key={String(field.name)}
+          name={String(field.name)}
+          rules={
+            field.required
+              ? [
+                  {
+                    required: true,
+                    message: `Please input your ${field.placeholder}!`,
+                  },
+                ]
+              : []
+          }
+        >
+          {field.inputType === "textarea" ? (
+            <TextArea rows={6} placeholder={field.placeholder} />
+          ) : (
+            <Input placeholder={field.placeholder} />
+          )}
+        </Form.Item>
+      ))}
+    </div>
   );
 };
 
--- src/components/Collapse/SelectCollapse.tsx
+++ src/components/Collapse/SelectCollapse.tsx
@@ -1,55 +1,45 @@
 import React from "react";
 
-import { Collapse, Select, Form } from "antd";
-
-const { Panel } = Collapse;
+import { Select, Form } from "antd";
 
 interface SelectCollapseProps {
-  header: string;
   selectField: {
     name: string;
     placeholder: string;
-    label: React.ReactNode; // Use React.ReactNode for JSX elements
+    label: React.ReactNode;
+    required?: boolean;
     options: Array<{ key: string; value: string; name: string }>;
     onChange: (value: string) => void;
   };
-  onChange: (key: string | string[]) => void;
 }
 
-const SelectCollapse: React.FC<SelectCollapseProps> = ({
-  header,
-  selectField,
-  onChange,
-}) => {
+const SelectCollapse: React.FC<SelectCollapseProps> = ({ selectField }) => {
   return (
-    <Collapse defaultActiveKey={["0"]} onChange={onChange}>
-      <Panel header={header} key="1">
-        <p>{selectField.label}</p>
-        <div className="w-full flex justify-evenly gap-10">
-          <Form.Item
-            name={selectField.name}
-            className={`h-fit w-full m-0 p-0`}
-            rules={[
-              {
-                required: true,
-                message: `Please select ${selectField.placeholder}!`,
-              },
-            ]}
+    <div>
+      {selectField.label && <p>{selectField.label}</p>}
+      <div className="w-full flex flex-col">
+        <Form.Item
+          name={selectField.name}
+          rules={[
+            {
+              required: true,
+              message: `Please input your ${selectField.placeholder}!`,
+            },
+          ]}
+        >
+          <Select
+            placeholder={`${selectField.placeholder}`}
+            onChange={selectField.onChange}
           >
-            <Select
-              placeholder={`${selectField.placeholder}`}
-              onChange={selectField.onChange}
-            >
-              {selectField.options.map((option) => (
-                <Select.Option key={option.key} value={option.value}>
-                  {option.value}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-        </div>
-      </Panel>
-    </Collapse>
+            {selectField.options.map((option) => (
+              <Select.Option key={option.key} value={option.value}>
+                {option.value}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+      </div>
+    </div>
   );
 };
 
